75c1c67bc932bb53187c0be642c34bacb5fe7b7a
Artur Baumeister <Artur.baumeister@gmail.com>
2022-08-13 08:47:38 +0200

    Done - try to complete to next class

---
+++ starter_code/scripts/menu_btn.js
@@ -0,0 +1,7 @@
+const menuButton = document.getElementsByClassName(".toggle");
+const linkList = document.getElementsByClassName("nav-links");
+
+menuButton.addEventListener("click", () => {
+  linkList.classList.toggle("active");
+});
+addEventListener;
